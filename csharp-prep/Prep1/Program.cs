adf90c4e7f77aa0b35bd995174bcd57c999c2a72
Diego <edu21016@byui.edu>
2024-01-12 15:44:37 -0700

    prep1 using strings and console write functions.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string f_name = Console.ReadLine();
+        Console.Write("What is your middle name? ");
+        string m_name = Console.ReadLine();
+
+        Console.WriteLine($"I really like your first name {f_name} and your middle name {m_name}.");   
+        Console.Write($"{f_name} {m_name} your amazing, have a wonderful day.");    
     }
 }
