23cac53bac364813791f6caf8bc6f11916b52aad
Diego <edu21016@byui.edu>
2024-01-19 12:59:51 -0700

    Finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.Write("Enter a list of numbers, type 0 when finished");
+        List<int> numbers = new List<int>();
+        int numbersList = -1;
+
+        while (numbersList != 0)
+        {
+            Console.Write("To be stop, press 0");
+            string response = Console.ReadLine();
+            numbersList = int.Parse(response);
+
+            if (numbersList != 0)
+            {
+                numbers.Add(numbersList);
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.Write($"The sum is: {sum}");
+         float average = ((float)sum) / numbers.Count;
+         Console.WriteLine($"The average is: {average}");
+
+         int max = numbers[0];
+
+         foreach (int number in numbers)
+         {
+            if(number > max){
+                max = number;
+            }
+         }
+
+         Console.Write($"We have foun a new max: {max}");
     }
 }
